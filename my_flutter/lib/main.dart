03d03f82b9b512b860fa12f60ca8dfcefbd36317
NaumovichID <naumova_i_d@sc.vsu.ru>
2023-05-17 18:14:24 +0300

    func-init: fix navbar color and animation

--- my_flutter/lib/main.dart
+++ my_flutter/lib/main.dart
@@ -4,34 +4,37 @@ import 'package:my_flutter/SearchPage.dart';
 
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My App',
       theme: ThemeData(
-        colorScheme: ColorScheme.dark(),
+        colorScheme: const ColorScheme.dark(),
       ),
-      home: NavigationPage(),
+      home: const NavigationPage(),
     );
   }
 }
 
 class NavigationPage extends StatefulWidget {
+  const NavigationPage({super.key});
+
   @override
   _NavigationPageState createState() => _NavigationPageState();
 }
 
 class _NavigationPageState extends State<NavigationPage> {
   int _selectedIndex = 0;
-  List<Widget> _pages = [HomePage(), SearchPage(), AccountPage()];
-  List<Widget> _allpages = [HomePage(), RecsPage(), SearchPage(), TicketsPage(), AccountPage()];
-  List<Widget> _appbarText = [Text('Home'), Text('Search'), Text('Account')];
-  List<Widget> _allappbarText = [Text('Home'), Text('Reccommendations'),
-    Text('Search'), Text('My tickets'), Text('Account')];
+  final List<Widget> _pages = [HomePage(), RecsPage(),
+    SearchPage(), TicketsPage(), AccountPage()];
+  final List<Widget> _appbarText = [const Text('Home'), const Text('Recommendations'),
+    const Text('Search'), const Text('My tickets'), const Text('Account')];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -49,41 +52,41 @@ class _NavigationPageState extends State<NavigationPage> {
 
       bottomNavigationBar: Container(
         margin: const EdgeInsets.all(16.0), // Устанавливаем отступы
-        decoration: BoxDecoration(
-          color: Colors.pinkAccent, // Цвет фона навигационной панели
+        child: ClipRRect(
           borderRadius: BorderRadius.circular(30.0), // Радиус скругления краев
-        ),
-        child: BottomNavigationBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0, // Устанавливаем значение elevation на 0, чтобы убрать тень
-          fixedColor: Colors.black, // Устанавливаем цвет активной кнопки
-          unselectedItemColor: Colors.black26, // Устанавливаем цвет неактивных кнопок
-          showSelectedLabels: false, // Устанавливаем значение false, чтобы скрыть выбранные метки
-          showUnselectedLabels: false, // Устанавливаем значение false, чтобы скрыть не выбранные метки
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(FontAwesomeIcons.house),
-              label: 'Home',
-            ),
-            // BottomNavigationBarItem(
-            //   icon: Icon(FontAwesomeIcons.list),
-            //   label: 'Recs',
-            // ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: 'Search',
-            ),
-            // BottomNavigationBarItem(
-            //   icon: Icon(FontAwesomeIcons.ticket),
-            //   label: 'Tickets',
-            // ),
-            BottomNavigationBarItem(
-              icon: Icon(FontAwesomeIcons.user),
-              label: 'Account',
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
+          child: BottomNavigationBar(
+            backgroundColor: Colors.pink,
+            type: BottomNavigationBarType.fixed, // Отменяем анимацию навбара
+            elevation: 0, // Устанавливаем значение elevation на 0, чтобы убрать тень
+            fixedColor: Colors.black, // Устанавливаем цвет активной кнопки
+            unselectedItemColor: Colors.black26, // Устанавливаем цвет неактивных кнопок
+            showSelectedLabels: false, // Устанавливаем значение false, чтобы скрыть выбранные метки
+            showUnselectedLabels: false, // Устанавливаем значение false, чтобы скрыть не выбранные метки
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(FontAwesomeIcons.house),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(FontAwesomeIcons.list),
+                label: 'Recs',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: 'Search',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(FontAwesomeIcons.ticket),
+                label: 'Tickets',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(FontAwesomeIcons.user),
+                label: 'Account',
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+          ),
         ),
       ),
     );
@@ -91,35 +94,43 @@ class _NavigationPageState extends State<NavigationPage> {
 }
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Text('Home Page'),
     );
   }
 }
 
 class RecsPage extends StatelessWidget {
+  const RecsPage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Text('Recs Page'),
     );
   }
 }
 
 class TicketsPage extends StatelessWidget {
+  const TicketsPage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Text('Tickets Page'),
     );
   }
 }
 class AccountPage extends StatelessWidget {
+  const AccountPage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Text('Account Page'),
     );
   }
