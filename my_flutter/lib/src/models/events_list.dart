6f81465e5d2acd44acd461897a85b6d879eda276
79969 <m.byckoff2015@yandex.ru>
2023-05-19 00:23:28 +0300

    ref: change entity name to EventModel

--- my_flutter/lib/src/models/events_list.dart
+++ my_flutter/lib/src/models/events_list.dart
@@ -1,11 +1,11 @@
 import 'package:my_flutter/src/models/event_model.dart';
 
 class EventsList {
-  late final List<event_model> events;
+  late final List<EventModel> events;
 
 
   EventsList.fromJson(List<dynamic> json) {
-  events = json.map((i)=>event_model.fromJson(i as Map<String, dynamic>)).toList();
+  events = json.map((i)=>EventModel.fromJson(i as Map<String, dynamic>)).toList();
   }
   EventsList(
       {
