19aa9c24b3e0925d35829c6f75cd07f2d58deac6
NaumovichID <naumova_i_d@sc.vsu.ru>
2023-06-01 13:17:02 +0300

    feat: add exchange details page

--- my_flutter/lib/src/ui/exchange/exchange_details.dart
+++ my_flutter/lib/src/ui/exchange/exchange_details.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../../models/request_model.dart';
+import '../custom_widgets/event_card.dart';
+import 'exchange_page.dart';
 
 class ExchangeDetailsPage extends StatefulWidget {
   final RequestModel exchange;
@@ -15,9 +17,91 @@ class _ExchangeDetailsPageState extends State<ExchangeDetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(38, 38, 38, 1),
-      body: SingleChildScrollView(
-        child: Column(),
+      backgroundColor: Theme.of(context).canvasColor,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).canvasColor,
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_rounded,
+            color: Theme.of(context).primaryColor,
+            size: 30,
+          ),
+          onPressed: () async {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Align(
+          alignment: AlignmentDirectional(-0.6, 0),
+          child: Text(
+            'Обмен билетов',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 22,
+            ),
+          ),
+        ),
+        actions: [],
+        centerTitle: false,
+        elevation: 2,
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(16, 50, 16, 25),
+            child: Text(
+              widget.exchange.userId as String, //todo: bloc
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(16, 0, 16, 16),
+            child: Text(
+              widget.exchange.description!,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(16, 0, 16, 16),
+            child: Text(
+              'Место автора: ${widget.exchange.seatFromUser}',
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(16, 0, 16, 16),
+            child: Text(
+              'Желаемое место: ${widget.exchange.wantedSeat}',
+            ),
+          ),
+          Row(children: [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(16, 8, 16, 16),
+                child: TextButton(
+                  onPressed: () async {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ExchangePage(),
+                      ),
+                    );
+                  },
+                  style: flatroundedButtonStyle,
+                  child: const Text(
+                    'Обменяться',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ]),
+        ],
       ),
     );
   }
---
+++ my_flutter/lib/src/ui/exchange/exchange_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ExchangePage extends StatefulWidget {
+  const ExchangePage({super.key});
+
+  @override
+  _ExchangePageState createState() => _ExchangePageState();
+}
+
+class _ExchangePageState extends State<ExchangePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(' ');
+  }
+}
