1891ef36eacd872141910d674f8560d01c091920
NaumovichID <naumova_i_d@sc.vsu.ru>
2023-06-03 14:25:03 +0300

    feat: tickets page rendering for different users

--- my_flutter/lib/src/ui/tickets_page.dart
+++ my_flutter/lib/src/ui/tickets_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:my_flutter/src/models/ticket_model.dart';
 
+import '../blocs/ticket_bloc.dart';
+import '../models/lists/tickets_list.dart';
+import '../models/user_model.dart';
+import '../resources/util/flutter_session.dart';
 import 'custom_widgets/ticket_card.dart';
 
 class TicketsWidget extends StatefulWidget {
@@ -11,12 +14,24 @@ class TicketsWidget extends StatefulWidget {
 }
 
 class _TicketsWidgetState extends State<TicketsWidget> {
-  TicketModel ticketPlaceholder = TicketModel(id: 0, price: 0, seat: '0',
-      status: '0', purchaseDate: '0', userId: 0, eventId: 0);
+  late UserModel user;
+
+  Future<UserModel> _fetchCurrentUser() async {
+    user = await FlutterSession().get("currentUser");
+    return user;
+  }
+
+  Future<void> _getUserTickets(int id) async {
+    bloc.fetchTicketsByUserId(id);
+  }
+
   @override
   void initState() {
     super.initState();
-    //todo: fetch user tickets
+    _fetchCurrentUser();
+    if(user.username != null) {
+      _getUserTickets(user.id!);
+    }
   }
 
   @override
@@ -34,14 +49,35 @@ class _TicketsWidgetState extends State<TicketsWidget> {
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Padding(
-              padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 16),
-              child: TicketCardWidget(ticket: ticketPlaceholder,),
-            ),
+            if (user.username != null) ...[
+              Expanded(
+                  child: StreamBuilder(
+                    stream: bloc.tickets,
+                    builder: (context, AsyncSnapshot<TicketsList> snapshot) {
+                      if (snapshot.hasData) {
+                        return ListView(
+                            children: List.generate(
+                                snapshot.data!.tickets.length,
+                                    (index) => TicketCardWidget(
+                                    ticket: snapshot.data!.tickets[index])));
+                      } else if (snapshot.hasError) {
+                        return Text(snapshot.error.toString());
+                      } else {
+                        return const Center(child: Text('Нет билетов ...'));
+                      }
+                    },
+                  )),
+            ] else ...[
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 16),
+                child: Text(
+                    "Зарегистрируйтесь или войдите, чтобы просматривать свои билеты",
+                style: TextStyle(color: Theme.of(context).primaryColorLight),),
+              ),
+            ]
           ],
         ),
       ),
-
     );
   }
 }
